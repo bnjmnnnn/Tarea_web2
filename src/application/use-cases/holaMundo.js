0e35cbb8387004840586b6ebde8d31f67c686014
bntkpp <thewild81@gmail.com>
2026-05-04 22:31:26 -0400

    agregue caso de uso "diHolaMundo."

---
+++ src/application/use-cases/holaMundo.js
@@ -0,0 +1,17 @@
+// La estructura de los casos de uso siempre sera la misma, se debe crear una clase con el nombre del caso de uso, en este caso "diHolaMundo", y dentro de la clase se debe crear un metodo llamado "execute" que es el que se va a ejecutar cuando se llame al caso de uso, este metodo debe retornar el resultado del caso de uso, en este caso "Hola Mundo".
+
+// 1. La Clase 
+class diHolaMundo {
+
+    // 2. El Constructor
+    constructor() {
+    }
+
+    //3. El Metodo Execute
+    async execute({nombre}) {
+        return `Hola ${nombre}! Bienvenido a Clean Architecture.`;
+    }
+}
+
+// 4. Exportar la Clase
+module.exports = diHolaMundo;
